7a43c6e08a4b6da2e20771b7c266f04d19d26912
Phil-Welsh <pwelsh8@gmail.com>
2022-02-16 12:17:30 -0800

    Added goals

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import routes from './config/routes';
-import { Chart as ChartJS } from 'chart.js/auto'
-import { Chart } from 'react-chartjs-2'
+
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 import './App.css';
